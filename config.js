e8df35011ef52d79b1ad7d0f38958d915e7c2c7e
CHORUS Labs <choruslabs.ai@gmail.com>
2026-07-03 07:01:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configure the Apps Script webapp base URL (deployed web app URL)
-const API_BASE = 'https://script.google.com/macros/s/AKfycbyTch_zPGtIBq69acuZfN0IzYTErj3hbqK6i-4P_UgmqS26Y8g1Nx5ZA8KbdoCIMJ0C/exec';
+const API_BASE = 'https://script.google.com/macros/s/AKfycbyO2R2dJi7vdUfTHu-HjFB8mPeJebDQ8QjG4CID3uiF2O6JjDVVpknZgMOCvrxNyrh0/exec';
 
 // Log to confirm config loaded
 console.log('✅ config.js loaded');
